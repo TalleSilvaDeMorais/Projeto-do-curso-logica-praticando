a70c565c3caea08376e84cca965683a1e5304c10
Talles silva de morais <moraistallessm@gmail.com>
2024-05-09 21:33:43 -0300

    carrinho finalizado

--- 3411-praticando-logica-projeto_inicial/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ 3411-praticando-logica-projeto_inicial/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,24 +1,35 @@
+let somaTotal = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = '$0';
 function adicionar(){
-    //recuperar valores, nome do produto, quantidade e valor
-    //calcular o preco do, o subTotal 
-    //adicionar no carrinho
-    //atualizar o valor total
-
 
     //recuperar valores, nome do produto, quantidade e valor
     let product = document.getElementById(`produto`).value;
     let name = product.split('-')[0];
     let value = product.split('R$')['1'];
     let qtd = document.getElementById('quantidade');
-
+    
+    //calcular o preco do, o subTotal 
     let soma = qtd.value * value;
     console.log(soma);
 
+    let carrinho = document.getElementById("lista-produtos");
+    
+    //adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${qtd.value}x</span> ${name} <span class="texto-azul">R$${soma}</span>
+  </section>`;
+    //atualizar o valor total
 
+    somaTotal = somaTotal + soma;
+    let CampoTotal = document.getElementById('valor-total');
+    CampoTotal.textContent = `R$ ${somaTotal}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar(){
     //limpar qtd, nome, preco
-
-
+    somaTotal = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = '$0';
 }
